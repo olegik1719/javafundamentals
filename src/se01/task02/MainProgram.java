1cea9adcbd5291f881ce109bc47853e3c531f417
olegik1719 <oleg.mihaylov@gmail.com>
2017-09-28 19:12:21 +0300

    Add function "Get epsilon" for getting that from user

--- src/se01/task02/MainProgram.java
+++ src/se01/task02/MainProgram.java
@@ -1,26 +1,42 @@
 package se01.task02;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainProgram {
     public static void main(String... args){
-        int minNumber = 0;
-        double epsilon = 0.01;
+        int minNumber = 1;
+        double epsilon = getEpsilon(args);//0.01;
         ArrayList<Double> sequence = new ArrayList<Double>();
-        System.out.println("Enter epsilon:");
-        for (double result=series(minNumber);result>=epsilon;minNumber++,result=series(minNumber)){
-            sequence.add(result);
-        }
+        //
+        double result;
+        for (sequence.add(result=series(minNumber));result >= epsilon;++minNumber,sequence.add(result=series(minNumber))){}
         System.out.println(minNumber);
         for(double d:sequence){
             System.out.println(d);
         }
     }
+
     private static double series(int n){
         double result = Double.MAX_VALUE;
         if (n!=-1){
             result = (double) 1/((n+1)*(n+1));
         }
         return result;
+    }
+
+    private static double getEpsilon(String[] args){
+        double eps = 0.01;
+        // if user get us parameters, then first try use as epsilon
+        if (args.length > 0){
+            eps = Double.parseDouble(args[0]);
+        }else{
+            System.out.println("Enter epsilon:");
+            Scanner sc = new Scanner(System.in);
+            if (sc.hasNextDouble()){
+                eps = sc.nextDouble();
+            }
+        }
+        return eps;
     }
 }
