1e00cbf88212aee808f3cfb85d551efd7b65a185
olegik1719 <oleg.mihaylov@gmail.com>
2017-09-29 14:34:38 +0300

    Task03. First simple version, without any benefits.

--- src/se01/task03/MainTask03.java
+++ src/se01/task03/MainTask03.java
@@ -2,7 +2,6 @@ package se01.task03;
 
 public class MainTask03 {
     public static void main(String[] args){
-        //double a = 1/0;
         double a = 0;
         double b = 1;
         double h = 0.1;
