908e6d6bd58aa541c2f2e7da8f6b1d95ef3777a0
olegik1719 <oleg.mihaylov@gmail.com>
2017-09-29 14:23:12 +0300

    Task03. First simple version, without any benefits.

--- src/se01/task03/MainTask03.java
+++ src/se01/task03/MainTask03.java
@@ -2,6 +2,17 @@ package se01.task03;
 
 public class MainTask03 {
     public static void main(String[] args){
+        //double a = 1/0;
+        double a = 0;
+        double b = 1;
+        double h = 0.1;
+        for (double x = a; x<= b; x+=h){
+            System.out.printf("%f | %f \n", x, getFunction(x));
+        }
 
+    }
+
+    private static double getFunction(double x){
+        return Math.tan(2*x) - 3;
     }
 }
