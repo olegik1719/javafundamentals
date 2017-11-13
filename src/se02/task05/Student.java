5299a04fc00d1edf4a909eca29253f34e4389d34
olegik1719 <32205310+olegik1719@users.noreply.github.com>
2017-11-13 17:37:21 +0300

    Update Student.java
    almost se02.task05

--- src/se02/task05/Student.java
+++ src/se02/task05/Student.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 public class Student {
+    
     @Getter
     @Setter
     private String name;
@@ -11,5 +12,35 @@ public class Student {
     public Student(String name){
         this.setName(name);
     }
-
+    /*
+    public boolean isInList(Subject subject){
+        return (студент в списке)
+    }
+    
+    public boolean isInList(Subject subject){
+        return (студент в списке)
+    }
+    
+    public Number gradeSubject(Subject subject){
+        if (isInList){
+            return (subject.getGrade(this))
+        }
+        return null
+    }
+    
+    public double averageGrade(){
+        Subject[] subjects = Subject.values();
+        int countSubj = 0;// Yes, it's default, I know
+        double sumGrades = 0; // Yes, it's default too
+        for (int i=0; i < subjects.length; i++){
+            if isInList(subjects[i]){
+                countSubj++;
+                sumGrades+=gradeSubject(subjects[i]);
+            }
+        }
+        if (countSubj == 0) return -1
+        else return sumGrades / countSubj
+    }
+    
+    */
 }
