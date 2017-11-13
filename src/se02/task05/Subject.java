c65bbc8c714d29abdba56587a251c55eea90cdf8
olegik1719 <32205310+olegik1719@users.noreply.github.com>
2017-11-13 17:31:24 +0300

    Update Subject.java
    getter isGradeInt

--- src/se02/task05/Subject.java
+++ src/se02/task05/Subject.java
@@ -1,10 +1,12 @@
 package se02.task05;
+import lombok.Getter;
 
 public enum Subject {
     MATHS(false),
     LANG(true),
     IT(false);
-
+    
+    @Getter
     private boolean isGradeInt;
 
     Subject(boolean isInt){
