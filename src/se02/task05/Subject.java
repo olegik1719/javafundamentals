8dace4a6231b368572b5acf50fd23b089218c7ca
olegik1719 <32205310+olegik1719@users.noreply.github.com>
2017-11-13 17:55:39 +0300

    Update Subject.java

--- src/se02/task05/Subject.java
+++ src/se02/task05/Subject.java
@@ -12,4 +12,46 @@ public enum Subject {
     Subject(boolean isInt){
         isGradeInt = isInt;
     }
+    /*
+    
+     private class RecordToList{
+        Student student;
+        Number grade;
+        RecordToList(Student student, Number grade){
+            this.student = student;
+            this.grade = grade;
+        }
+        RecordToList changeGrade(Number grade){
+            this.grade = grade;
+            return this;
+        }
+     }
+     
+     private ArrayList<RecordToList> records;
+     
+     public Subject addStudent(Student student){
+        return addGrade(student, 0)
+     }
+     
+     public Subject addRecord(RecordToList record){
+        // add ...
+     }
+     
+     public Subject addGrade(Student student, Number grade){
+        if (student isn't in List){
+            RecordToList record = new RecordToList(student,0);
+            addRecord(record);
+        }else{
+            // change record
+            
+        }
+        return this;
+     }
+     //Number getGrade(student){
+     // if isInt Number.toInt
+     // else Num.toDouble
+     
+     //public boolean isHere(student);
+     
+    */
 }
