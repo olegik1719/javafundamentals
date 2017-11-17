39c5b28cd66ca958c8177b343552fa0524119f4d
olegik1719 <32205310+olegik1719@users.noreply.github.com>
2017-11-17 16:33:50 +0300

    Add files via upload

---
+++ src/se04/task04/Film.java
@@ -0,0 +1,36 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Film implements Serializable{
+	private String title;
+	private ArrayList<Actor> actors;
+
+	public Film(String title, String[] actors){
+		this.title = title;
+		//System.out.printf("title: %s\n", this.title);
+		this.actors = new ArrayList<>();
+		for (String name: actors){
+			//System.out.printf("actor: %s\n", name);
+			this.actors.add(Actor.getActor(name));
+		}
+	}
+
+	public Film(String title){
+		this.title = title;
+		//System.out.printf("title: %s\n", this.title);
+		this.actors = new ArrayList<>();
+	}
+
+	public String toString(){
+		String result = title + '\n';
+		if (actors.size() > 0) {
+			result = result + "\tStarring:\n";
+			for (Actor actor: actors){
+				result += "\t\t" + actor.getName() + '\n';
+			}
+		}
+		return result;
+	}
+
+
+}
